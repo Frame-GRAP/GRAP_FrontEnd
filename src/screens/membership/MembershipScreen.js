2de581f0e9e0ddac82fef087c581eb9aa07fd1c1
chan <baewonchan1@naver.com>
2021-05-27 00:44:16 +0900

    Modify css and add get price from element.

--- src/screens/membership/MembershipScreen.js
+++ src/screens/membership/MembershipScreen.js
@@ -4,55 +4,11 @@ import {useSelector} from "react-redux";
 import {selectUser} from "../../features/userSlice";
 import Footer from "../../Footer";
 import Nav from "../../Nav";
-import {
-    Button,
-    Card,
-    CardActions,
-    CardContent,
-    CardHeader, Checkbox,
-    Container,
-    Grid,
-    makeStyles,
-    Typography
-} from "@material-ui/core";
-import './MembershipScreen.css';
-
-
-const useStyles = makeStyles((theme) => ({
-    '@global': {
-
-    },
-    card:{
-        background: '#252525',
-    },
-    cardTitle:{
-        color: '#fff',
-    },
-    cardHeader: {
-        color: '#fff',
-    },
-    cardPricing: {
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'baseline',
-        marginBottom: theme.spacing(2),
-        color: '#fff',
-    },
-    cardDescription: {
-        display: 'flex',
-        listStyle: 'none',
-        justifyContent: 'center',
-        alignItems: 'baseline',
-        marginBottom: theme.spacing(2),
-        color: '#fff',
-    },
-    cardCheck: {
-        textAlign: 'center',
-        marginBottom: theme.spacing(2),
-        justifyContent: 'center',
-        color: '#fff',
-    }
-}));
+import './Membership2.css';
+import {useHistory} from "react-router-dom";
+import bronze from '../../img/bronze-medal.png';
+import silver from '../../img/silver-medal.png';
+import gold from '../../img/gold-medal.png';
 
 const tiers = [
     {
@@ -80,9 +36,47 @@ const tiers = [
 
 
 function MembershipScreen() {
-    const classes = useStyles();
     const user = useSelector(selectUser);
     const [loading, setLoading] = useState(true);
+    const history = useHistory();
+
+    const { IMP } = window;
+    IMP.init('imp40158151');
+
+    const payment = () => {
+        const price = RegisterMembership();
+
+        /*IMP.request_pay({
+            pay_method : 'card', // 결제창 호출단계에서의 pay_method 는 아무런 역할을 하지 못하며, 구매자가 카카오페이 앱 내에서 신용카드 vs 카카오머니 중 실제 선택한 값으로 추후 정정됩니다.
+            merchant_uid : 'merchant_' + new Date().getTime(),
+            name : '최초인증결제',
+            amount : 1004, // 빌링키 발급과 동시에 1,004원 결제승인을 시도합니다.
+            customer_uid : 'your-customer-unique-id', //customer_uid 파라메터가 있어야 빌링키 발급이 정상적으로 이뤄집니다.
+            buyer_email : 'iamport@siot.do',
+            buyer_name : '아임포트',
+            buyer_tel : '02-1234-1234'
+        }, rsp => {
+            if (rsp.success) {
+                // 빌링키 발급 성공
+                // axios로 HTTP 요청
+                axios({
+                    url: "https://www.myservice.com/billings/", // 서비스 웹서버
+                    method: "post",
+                    headers: { "Content-Type": "application/json" },
+                    data: {
+                        customer_uid: "gildong_0001_1234" // 카드(빌링키)와 1:1로 대응하는 값
+                    }
+                }).then((r) => {
+                    console.log(r.data);
+                    alert("결제에 성공하였습니다.");
+                    history.push("/")
+                })
+            } else {
+                // 빌링키 발급 실패
+                alert("결제에 실패하였습니다.");
+            }
+        });*/
+    };
 
     useEffect(() => {
         setLoading(false);
@@ -91,6 +85,29 @@ function MembershipScreen() {
         }
     }, []);
 
+    const getTierImg = (index) => {
+        if(index == 0)
+            return (<img src={bronze} alt="bronze"/>)
+        if(index == 1)
+            return (<img src={silver} alt="bronze"/>)
+        if(index == 2)
+            return (<img src={gold} alt="bronze"/>)
+    }
+
+    function RegisterMembership() {
+        let price = "";
+        const checked = document.getElementsByName("membership_selectBtn");
+
+        checked.forEach((set) => {
+            if (set.checked === true) {
+                price = set.value;
+            }
+        })
+
+        return price;
+    }
+
+
     if(loading) return (<div>Loading...</div>);
     return (
         <>
@@ -98,74 +115,38 @@ function MembershipScreen() {
                 <Nav />
                 <div className="membershipScreen_body">
                     <h2>멤버쉽</h2>
+                    <h3>지금 바로 가입해서 당신이 원하는 게임을 마음껏 플레이 하세요!</h3>
                     <div className="membershipScreen_result">
-                        {/*<Container maxWidth="md" component="main">
-                            <Grid container spacing={5} alignItems="flex-end">
-                                {tiers.map((tier) => (
-                                    // Enterprise card is full width at sm breakpoint
-                                    <Grid item key={tier.title} xs={12} sm={tier.title === 'Enterprise' ? 12 : 6} md={4}>
-                                        <Card className={classes.card}>
-                                            <CardHeader
-                                                className={classes.cardTitle}
-                                                title={tier.title}
-                                                titleTypographyProps={{ align: 'center' }}
-                                            />
-                                            <CardContent>
-                                                <ul className={classes.cardDescription}>
-                                                    {tier.description.map((line) => (
-                                                        <Typography component="li" variant="subtitle1" align="center" key={line}>
-                                                            {line}
-                                                        </Typography>
-                                                    ))}
-                                                </ul>
-                                                <div className={classes.cardPricing}>
-                                                    <Typography component="h2" variant="h3">
-                                                        {tier.price}
-                                                    </Typography>
-                                                    <Typography variant="h6">
-                                                        원/월
-                                                    </Typography>
-                                                </div>
-                                            </CardContent>
-                                            <CardActions>
-                                                <Checkbox
-                                                    className={classes.cardCheck}
-                                                    color="primary"
-                                                    inputProps={{ 'aria-label': 'secondary checkbox' }}
-                                                />
-                                            </CardActions>
-                                        </Card>
-                                    </Grid>
-                                ))}
-                            </Grid>
-                        </Container>*/}
-                        <ul className="membership_group">
+                        <div className="membership_container">
                             {tiers.map((tier, index) => (
-                                <li className="membership_list">
-                                    <div className="membership_item">
-                                        <label htmlFor={tier.title}>
-                                            <h2 className="membership_name">{tier.title}<br/></h2>
-                                            <h3 className="membership_description">{tier.description}<br/></h3>
-                                            <h6 className="membership_price">{tier.price}/월</h6>
-                                            </label>
-                                        <input
-                                            key={index}
-                                            type="radio"
-                                            className="membership_check"
-                                            name="membership_selectBtn"
-                                            id={tier.title}
-                                            value={tier.title}
-                                        />
-                                    </div>
-                                </li>
-                            ))}
-                        </ul>
-                    </div>
+                                <label className="membership_item" htmlFor={tier.title}>
+                                    <input
+                                        key={index}
+                                        type="radio"
+                                        className="membership_check"
+                                        name="membership_selectBtn"
+                                        id={tier.title}
+                                        value={tier.price}
+                                        defaultChecked={index == 0}
+                                    />
+                                    <div className="item_inner">
+                                        <div className="membership_img">
+                                            {getTierImg(index)}
+                                        </div>
+                                        <h3 className="membership_name">{tier.title}<br/></h3>
+                                        <h4 className="membership_description">{tier.description}<br/></h4>
+                                        <h6 className="membership_price">월 {tier.price}원</h6>
 
+                                    </div>
+                                </label>
+                            ))}
+                        </div>
+                    </div>
                     <div className="membership_submit">
-                        <button className="membership_btn">가입하기</button>
+                        <button className="membership_btn" onClick={payment}>가입하기</button>
                     </div>
                 </div>
+
             </div>
             <Footer />
         </>

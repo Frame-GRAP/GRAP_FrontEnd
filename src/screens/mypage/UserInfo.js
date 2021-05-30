26ea0d3c9f76a0f9534486ba1fba61b80c275a68
chan <baewonchan1@naver.com>
2021-05-31 01:49:34 +0900

    Change selected effect in signup page

--- src/screens/mypage/UserInfo.js
+++ src/screens/mypage/UserInfo.js
@@ -129,15 +129,15 @@ function UserInfo() {
                             return (
                                 <>
                                 <div className="question_answer_item">
-                                    <input
-                                        key={index}
-                                        type="checkbox"
-                                        className="question2_selectBtn"
-                                        name="question2_selectBtn"
-                                        id={set.id}
-                                        value={set.id}
-                                    />
                                     <label className="question2_gameImg" htmlFor={set.id}>
+                                        <input
+                                            key={index}
+                                            type="checkbox"
+                                            className="question2_check"
+                                            name="question2_selectBtn"
+                                            id={set.id}
+                                            value={set.id}
+                                        />
                                         <div className="img_container">
                                             <img src={set.headerImg} className="question2_gameImg"></img>
                                         </div>

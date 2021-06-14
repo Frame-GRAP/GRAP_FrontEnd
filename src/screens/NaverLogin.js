0d70500fd05f744864916471c0cc551f9e0517fd
chan <baewonchan1@naver.com>
2021-06-15 03:11:19 +0900

    Modify naver redirect url and add build file.

--- src/screens/NaverLogin.js
+++ src/screens/NaverLogin.js
@@ -47,7 +47,7 @@ function NaverLogin(){
     useEffect(() => {
         const naverLogin = new window.naver.LoginWithNaverId({
             clientId: "Fz2SCpCmnixzkftVz6Eb",
-            callbackUrl: "http://127.0.0.1:3000",
+            callbackUrl: "https://grap.p-e.kr",
             isPopup: false, // popup 형식으로 띄울것인지 설정
             loginButton: { color: 'green', type: 1, height: '50' }, //버튼의 스타일, 타입, 크기를 지정
         });

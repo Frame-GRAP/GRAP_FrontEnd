991112c6bf7eb1749ab2146f3d4d0cbece5a19f5
chan <baewonchan1@naver.com>
2021-06-14 16:18:47 +0900

    Add naver and kakao login.

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, {useEffect, useRef} from "react";
 import "./LoginScreen.css"
 import grap_logo from '../img/grap_logo2-1.png';
 import { useHistory } from "react-router-dom";
@@ -7,6 +7,7 @@ import GoogleLogin from 'react-google-login';
 import {useDispatch} from "react-redux";
 import {login} from "../features/userSlice";
 import Footer from "../Footer";
+import NaverLogin from "./NaverLogin";
 
 function LoginScreen(){
     const history = useHistory();
@@ -14,8 +15,50 @@ function LoginScreen(){
     const passwordRef = useRef(null);
     const dispatch = useDispatch();
 
-    const responseSuccess= (response) => {
-        const user = response.profileObj;
+
+    const kakaoLogin = () => {
+        window.Kakao.Auth.logout();
+        window.Kakao.Auth.login({
+            success: (auth) => {
+                window.Kakao.API.request({
+                    url: "/v2/user/me",
+                    success: (res) => {
+                        console.log(res.kakao_account.email);
+                        console.log(res.kakao_account.profile.nickname);
+                        console.log(res.kakao_account.profile.profile_image_url);
+                        responseSuccess(res.kakao_account, "kakao");
+                    },
+                    fail: (err) => {
+                        console.log(err);
+                    }
+                })
+            },
+            fail: (err) => {
+                console.log(err);
+            }
+        })
+    }
+
+    const googleLogin = (response) => {
+        let user = {};
+        user = response.profileObj;
+        responseSuccess(user, "google");
+    }
+
+    const responseSuccess= (data, type) => {
+        let user = {};
+
+        if(type === "kakao"){
+            user.name = data.profile.nickname;
+            user.email = data.email;
+            user.imageUrl = data.profile.profile_image_url;
+        }
+        else if(type === "google"){
+            user.name = data.name;
+            user.email = data.email
+            user.imageUrl = data.imageUrl;
+        }
+
         axios({
             method: 'post',
             url: 'http://ec2-3-35-250-221.ap-northeast-2.compute.amazonaws.com:8080/api/user',
@@ -29,24 +72,23 @@ function LoginScreen(){
                 console.log(res.data)
 
                 axios.get(`http://ec2-3-35-250-221.ap-northeast-2.compute.amazonaws.com:8080/api/user/${res.data.id}`)
-                .then((res)=>{
-                    console.log(res.data);  
-                    dispatch(login({
-                        user_id : res.data.id,
-                        name : user.name,
-                        nickname: res.data.nickname,
-                        membershipName : res.data.membershipName,
-                        availableCoupon: res.data.availableCoupon,
-                        nextPaymentDay: res.data.nextPaymentDay
-                    }))
-                    window.localStorage.setItem("user_id", JSON.stringify(res.data.id));
-                    window.localStorage.setItem("name", user.name);
-                    window.localStorage.setItem("nickname", res.data.nickname);
-                    window.localStorage.setItem("membershipName", res.data.membershipName);
-                    window.localStorage.setItem("availableCoupon", res.data.availableCoupon);
-                    window.localStorage.setItem("nextPaymentDay", res.data.nextPaymentDay);
-                
-                })
+                    .then((res)=>{
+                        console.log(res.data);
+                        dispatch(login({
+                            user_id : res.data.id,
+                            name : user.name,
+                            nickname: res.data.nickname,
+                            membershipName : res.data.membershipName,
+                            availableCoupon: res.data.availableCoupon,
+                            nextPaymentDay: res.data.nextPaymentDay
+                        }))
+                        window.localStorage.setItem("user_id", JSON.stringify(res.data.id));
+                        window.localStorage.setItem("name", user.name);
+                        window.localStorage.setItem("nickname", res.data.nickname);
+                        window.localStorage.setItem("membershipName", res.data.membershipName);
+                        window.localStorage.setItem("availableCoupon", res.data.availableCoupon);
+                        window.localStorage.setItem("nextPaymentDay", res.data.nextPaymentDay);
+                    })
 
                 if(res.data.isRegistered == 1 && res.data.nickname !== null){
                     history.push("/");
@@ -62,7 +104,7 @@ function LoginScreen(){
 
     const responseFail = (err) => {
         console.log(err.error);
-        alert(err);
+        alert(err.error);
     }
 
     return (
@@ -91,21 +133,24 @@ function LoginScreen(){
                     <span className="info_gray" onClick={() => history.push("/register")}>회원가입</span>
                 </div>
                 <div className="loginScreen_sns">
-                    <a href="">
-                        <span className="sns_icon naver"></span>
-                    </a>
-                    <a href="">
-                        <span className="sns_icon kakao"></span>
-                    </a>
+                    <div className="sns_icon">
+                        <NaverLogin />
+                        {/*<div id="naverIdLogin" className="sns_icon naver"/>*/}
+                    </div>
+                    <div className="sns_icon kakao" onClick={kakaoLogin}>
+                        {/*<img className="sns_icon kakao" src={kakaoLoginImg} alt=""/>*/}
+                        <span className="kakao"></span>
+                    </div>
                     <GoogleLogin
                         className="sns_icon google"
                         clientId="803232667536-laafp22eio1k1kl9qrj4jabrusq1vo81.apps.googleusercontent.com"
                         render={renderProps => (
                             <span onClick={renderProps.onClick} disabled={renderProps.disabled} className="sns_icon google"></span>
                         )}
-                        onSuccess={responseSuccess}
+                        onSuccess={googleLogin}
                         onFailure={responseFail}
                     />
+
                 </div>
             </div>
             <Footer />
---
+++ src/screens/NaverLogin.js
@@ -0,0 +1,122 @@
+import React, {useEffect} from "react";
+import axios from "axios";
+import {login} from "../features/userSlice";
+import {useHistory} from "react-router-dom";
+import {useDispatch} from "react-redux";
+
+function NaverLogin(){
+    const history = useHistory();
+    const dispatch = useDispatch();
+
+    /*const Naver = () => {
+        const naverLogin = new window.naver.LoginWithNaverId({
+            clientId: "Fz2SCpCmnixzkftVz6Eb",
+            callbackUrl: "http://127.0.0.1:3000",
+            isPopup: false, // popup 형식으로 띄울것인지 설정
+            loginButton: { color: 'green', type: 1, height: '50' }, //버튼의 스타일, 타입, 크기를 지정
+        });
+        naverLogin.init();
+    }
+
+    const UserProfile = () => {
+        window.location.href.includes('access_token') && GetUser();
+        function GetUser() {
+            const location = window.location.href.split('=')[1];
+            const token = location.split('&')[0];
+
+            const header = "Bearer " + token; // Bearer 다음에 공백 추가
+            console.log(header);
+            axios({
+                method: "GET",
+                url: 'https://openapi.naver.com/v1/nid/me',
+                headers : {
+                    "Content-type" : "application/json",
+                    "Authorization": header
+                },
+            })
+                .then(res => {
+                    console.log(res);
+                })
+                .catch(err => console.log("err : ", err));
+        }
+    };*/
+
+
+
+
+    useEffect(() => {
+        const naverLogin = new window.naver.LoginWithNaverId({
+            clientId: "Fz2SCpCmnixzkftVz6Eb",
+            callbackUrl: "http://127.0.0.1:3000",
+            isPopup: false, // popup 형식으로 띄울것인지 설정
+            loginButton: { color: 'green', type: 1, height: '50' }, //버튼의 스타일, 타입, 크기를 지정
+        });
+        naverLogin.init();
+        naverLogin.logout();
+        naverLogin.getLoginStatus((status) => {
+            if(status) {
+                let user = {};
+                console.log(naverLogin);
+                console.log(naverLogin.user.name);
+                console.log(naverLogin.user.email);
+                console.log(naverLogin.user.profile_image);
+
+                user.name = naverLogin.user.name;
+                user.email = naverLogin.user.email;
+                user.imageUrl = naverLogin.user.profile_image;
+
+                axios({
+                    method: 'post',
+                    url: 'http://ec2-3-35-250-221.ap-northeast-2.compute.amazonaws.com:8080/api/user',
+                    data: {
+                        name: user.name,
+                        email: user.email,
+                        picture: user.imageUrl
+                    }
+                }).then((res) => {
+                    if(res){
+                        console.log(res.data)
+
+                        axios.get(`http://ec2-3-35-250-221.ap-northeast-2.compute.amazonaws.com:8080/api/user/${res.data.id}`)
+                            .then((res)=>{
+                                console.log(res.data);
+                                dispatch(login({
+                                    user_id : res.data.id,
+                                    name : user.name,
+                                    nickname: res.data.nickname,
+                                    membershipName : res.data.membershipName,
+                                    availableCoupon: res.data.availableCoupon,
+                                    nextPaymentDay: res.data.nextPaymentDay
+                                }))
+                                window.localStorage.setItem("user_id", JSON.stringify(res.data.id));
+                                window.localStorage.setItem("name", user.name);
+                                window.localStorage.setItem("nickname", res.data.nickname);
+                                window.localStorage.setItem("membershipName", res.data.membershipName);
+                                window.localStorage.setItem("availableCoupon", res.data.availableCoupon);
+                                window.localStorage.setItem("nextPaymentDay", res.data.nextPaymentDay);
+                            })
+
+                        if(res.data.isRegistered == 1 && res.data.nickname !== null){
+                            history.push("/");
+                        }
+                        else{
+                            history.push("/userInfo");
+                        }
+                    }
+                    else
+                        alert("fail");
+                })
+
+            }
+            else{
+                console.log("not loggedin");
+            }
+        });
+    }, []);
+
+    return (
+        <div id="naverIdLogin" />
+    )
+}
+
+export default NaverLogin;
